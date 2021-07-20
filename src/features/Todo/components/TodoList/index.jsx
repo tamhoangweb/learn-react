36934e6cec3a8974517eac15bcab7de5df2c2243
tamhoangweb <tamhoangweb@gmail.com>
2021-07-21 04:02:42 +0700

    filtered todolist

--- src/features/Todo/components/TodoList/index.jsx
+++ src/features/Todo/components/TodoList/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import './styles.scss';
 
-const TodoList = ({ todoList }) => {
+const TodoList = ({ todoList, onTodoClick }) => {
   return (
     <ul>
       {
-        todoList.map(todo => (
-          <li key={todo.id}>{todo.title}</li>
+        todoList.map((todo, index) => (
+          <li onClick={() => { onTodoClick && onTodoClick(todo, index) }} className={classnames({ 'todo-item': true, completed: todo.status === 'completed' })} key={todo.id}>{todo.title}</li>
         ))
       }
     </ul>
@@ -15,6 +17,11 @@ const TodoList = ({ todoList }) => {
 
 TodoList.propTypes = {
   todoList: PropTypes.array,
+  onTodoClick: PropTypes.func,
+};
+TodoList.defaultProps = {
+  todoList: [],
+  onTodoClick: null,
 };
 
 export default TodoList;
--- src/features/Todo/index.jsx
+++ src/features/Todo/index.jsx
@@ -1,25 +1,52 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 import TodoList from './components/TodoList';
 
 const TodoFeature = props => {
-  const todoList = [
+  const initTodoList = [
     {
       id: 1,
       title: 'Eat',
+      status: 'completed'
     },
     {
       id: 2,
       title: 'Sleep',
+      status: 'new'
     },
     {
       id: 3,
       title: 'Code',
+      status: 'new'
     },
   ]
+  const [todoList, setTodoList] = useState(initTodoList)
+  const [filteredStatus, setFilteredStatus] = useState('all')
+
+  const filteredTodoList = todoList.filter((todo) => filteredStatus === todo.status || filteredStatus === 'all')
+  console.log(filteredTodoList)
+  function handleShowAll() {
+    setFilteredStatus('all')
+  }
+  function handleShowCompleted() {
+    setFilteredStatus('completed')
+  }
+  function handleShowNew() {
+    setFilteredStatus('new')
+  }
+  function handleTodoClick(todo, index) {
+    console.log('clicked', todo)
+    let newTodoList = [...todoList]
+    newTodoList[index].status === 'new' ? newTodoList[index].status = 'completed' : newTodoList[index].status = 'new'
+    setTodoList(newTodoList)
+  }
   return (
     <>
-      <TodoList todoList={todoList} />
+      <TodoList todoList={filteredTodoList} onTodoClick={handleTodoClick} />
+      <div>
+        <button onClick={handleShowAll}>Show All</button>
+        <button onClick={handleShowCompleted}>Show Completed</button>
+        <button onClick={handleShowNew}>Show New</button>
+      </div>
     </>
   );
 };
